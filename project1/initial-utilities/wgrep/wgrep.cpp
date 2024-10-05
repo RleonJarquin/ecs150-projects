75ff2af73fc784e979624d072c0cbadc5d1d36cd
Ruben Leon Jarquin <rjleon@ucdavis.edu>
2024-10-05 10:58:23 -0700

    Finished wgrep command

--- project1/initial-utilities/wgrep/wgrep.cpp
+++ project1/initial-utilities/wgrep/wgrep.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -7,27 +8,93 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
 
-	if(argc != 2){
-		cerr << "Error: Invalid number of arguments." << endl;
-		cerr << "Usage: " << argv[0] << " <file>" << endl;
-		return 1;		 
-	}
-	// Change this to accept multpile files. 	
-	int fileDescriptor = open(argv[1], O_RDONLY);
-	char* buff[4096];
-	int bytesRead;
 
-	if(fileDescriptor < 0){
-		cout << "cannot open file" << endl;
-		return 1;
+void find_pattern(const int patternSize, const char* pattern, const int fileDescriptor){
+	
+	int j = 0;
+	char buff[2]; 
+	string my_line = "";
+	const char null_char = '\0';
+	const char nl_char = '\n';
+	bool match = false;
+	buff[1] = null_char; 
+
+	// Process 4096 characters 
+	while (read(fileDescriptor, buff, 1) > 0){
+		// If there is a pattern match and the current character is not \n continue
+		my_line += buff[0];
+		if(match && buff[0] != nl_char){
+			continue;
+		}
+		else{
+			// Otherwise if the character is \n and the pattern matches write to STDOUT and reset
+			if(buff[0] == nl_char){
+				if(match){
+					const char* my_line_ptr = my_line.c_str(); 
+					write(1, my_line_ptr, my_line.length());
+				}
+				my_line = "";
+				match = false;
+				j = 0;
+				continue;
+			}
+			// Else check if buff[i] matches pattern[j]
+			if(buff[0] == pattern[j]){
+				j++;
+				if(patternSize == j){
+					match = true;
+				}	
+			}
+			else{
+				j = 0;
+			}
+		}
+		
 	}
 	
-	while ((bytesRead = read(fileDescriptor, buff, sizeof(buff)-1)) > 0){
-		write(1, buff, bytesRead);
-	}	
+	if(fileDescriptor != 0){
+		close(fileDescriptor);
+	}
+		return; 
+}
 
-	close(fileDescriptor);
-	return 0;
+	
+int main(int argc, char* argv[]){
+
+	if(argc == 1){
+		cout << "wgrep: searchterm [file ...]" << endl;
+		exit(1);
+	}
+
+	int patternSize = strlen(argv[1]);
+	char* pattern = argv[1];
+	bool file_passed;
+	int fileDescriptor;
+
+	if(argc == 2){
+		file_passed = false;
+	}
+	else{
+		file_passed = true;
+	}
+	
+	if(file_passed){
+		for(int file = 2; file < argc;file++){
+			fileDescriptor = open(argv[file], O_RDONLY);
+
+			if(fileDescriptor < 0){
+				cout << "wgrep: cannot open file" << endl;
+				exit(1);
+			}
+
+			find_pattern(patternSize, pattern, fileDescriptor);
+		}
+	}
+	else{
+		fileDescriptor = STDIN_FILENO;
+		find_pattern(patternSize, pattern, fileDescriptor);
+	}
+
+        return 0;
 }
